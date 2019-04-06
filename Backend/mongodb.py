47199f39be088b8fd46b239a3cc4a9a4d687b16f
ryan-beck <rbeck@csumb.edu>
2019-04-06 16:55:31 -0700

    create_entry functional

--- Backend/mongodb.py
+++ Backend/mongodb.py
@@ -26,19 +26,45 @@ class Food:
 class Ingredient:
     def __init__(self, name):
         self.name = name
-        d = get_info(name)
-        self.summary = d["summary"]
-        self.health = d["health"]
+        db = myclient.test
+        posts = db.ingredients
+        if posts.find_one({'title': name}) == None: # if ingredient not in database
+            d = get_info(name)
+            post_data = {
+                "name" : name,
+                "info" : d
+            }
+            result = posts.insert_one(post_data)
+
+
+        # self.summary = d["summary"]
+        # self.health = d["health"]
 
 
 def create_entry(name, ingredients):
-    db = myclient.test
-    newFood = Food(name, ingredients)
+    db = myclient.ingredientInfo
+    food_ingredients = db.ingredients
+    foods = db.foods
+
     food_post = {
         "name" : name,
         "ingredients" : []
     }
-    for i in newFood.ingredients:
+
+    for i in ingredients:
+        id = food_ingredients.find_one({'name': i})
+
+        if id == None: # if ingredient not in database
+            d = get_info(i)
+            post_data = {
+                "name" : i,
+                "info" : d
+            }
+            result = food_ingredients.insert_one(post_data)
+
+        food_post["ingredients"].append(i)
+    result = foods.insert_one(food_post)
+
 
 
 def postTest(term):
@@ -53,11 +79,28 @@ def postTest(term):
 
     result = posts.insert_one(post_data)
 
-postTest("glucose")
+lst = ["Water", "Malic Acid", "Absorbic Acid", "xanthan gum"]
+create_entry("other", lst)
 
-db = myclient.ingredient_info
-posts = db.posts
-print(posts.find_one({'title': 'glucose'})["_id"])
+# db = myclient.test2
+# posts = db.ingredients
+#
+# post_data = {
+#     "title" : "water",
+#     "info" : get_info("water")
+# }
+# result = posts.insert_one(post_data)
+
+# postTest("glucose")
+#
+# db = myclient.ingredient_info
+# posts = db.posts
+#
+# x = posts.find_one({'title': 'apple'})
+# if x == None:
+#     print("none")
+# else:
+#     print(x["_id"])
 # x = mycol.insert_one(post_data)
 #print(x)
 #print(myclient.list_database_names())
